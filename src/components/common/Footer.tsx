256f3b6ac77881da27ae8d3930e86868383bdd1b
karmakarCoder <dev.rodro@gmail.com>
2026-08-17 22:06:47 +0600

    footer update

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -2,7 +2,7 @@ export const Footer = () => {
   return (
     <footer className="bg-gray-950/30 backdrop-blur-xl p-8 md:px-12">
       <div className="text-xs font-mono opacity-40 capitalize text-center flex items-center justify-center">
-        © {new Date().getFullYear()} All rights reserved. Made by Rodro
+        © {new Date().getFullYear()} All rights reserved. Made by Rodro.
       </div>
     </footer>
   );
